3d6c3b927daa347a5c91a2dea7b15d6958ce143d
diggy <nitayama.biz@gmail.com>
2025-12-30 07:06:19 +0900

    fix: use full path for php and composer

--- public/install.php
+++ public/install.php
@@ -1,7 +1,9 @@
 <?php
-// ロリポップ上でcomposer installを実行するスクリプト
 echo "Starting installation...<br>";
-exec("composer install 2>&1", $output, $return_var);
+// ロリポップのPHP8.xのフルパスを指定
+$phpPath = '/usr/local/php/8.2/bin/php'; 
+exec("{$phpPath} /usr/local/bin/composer install 2>&1", $output, $return_var);
+
 echo "<pre>" . implode("\n", $output) . "</pre>";
 if ($return_var === 0) {
     echo "Installation successful!";
