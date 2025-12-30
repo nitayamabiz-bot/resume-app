a7fe9160459f9c4f03b72726fc36a0e45884a8ff
diggy <nitayama.biz@gmail.com>
2025-12-30 12:12:22 +0900

    mail

--- config/mail.php
+++ config/mail.php
@@ -39,7 +39,7 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
+            'scheme' => env('MAIL_SCHEME', env('MAIL_PORT') == 465 ? 'smtps' : (env('MAIL_PORT') == 587 ? 'smtp' : null)),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
