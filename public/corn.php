d23b64081f058918ebd63e1f4d0196561c6ef81c
diggy <nitayama.biz@gmail.com>
2025-12-30 15:09:57 +0900

    cron

--- public/corn.php
+++ public/corn.php
@@ -1,4 +1,4 @@
 <?php
-// パスを hamro-life-japan.com に合わせて修正
-exec('/usr/local/bin/php /home/users/1/littlestar.jp-proud-takeo-0732/web/hamro-life-japan.com/artisan fetch:nepal-news 2>&1', $output);
+// php8.4 を直接指定
+exec('php8.4 /home/users/1/littlestar.jp-proud-takeo-0732/web/hamro-life-japan.com/artisan fetch:nepal-news 2>&1', $output);
 print_r($output);
