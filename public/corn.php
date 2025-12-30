c9082342f7c4757e5c24da7111483f03c805cdf2
diggy <nitayama.biz@gmail.com>
2025-12-30 15:12:08 +0900

    croooon

--- public/corn.php
+++ public/corn.php
@@ -1,4 +1,11 @@
 <?php
-// フルパス指定版
-exec('/usr/local/php/8.4/bin/php /home/users/1/littlestar.jp-proud-takeo-0732/web/hamro-life-japan.com/artisan fetch:nepal-news 2>&1', $output);
+// ロリポップ PHP8.4 の正確なパスを指定
+$phpPath = '/usr/local/php/8.4/bin/php';
+$artisanPath = '/home/users/1/littlestar.jp-proud-takeo-0732/web/hamro-life-japan.com/artisan';
+$command = "{$phpPath} {$artisanPath} fetch:nepal-news 2>&1";
+
+exec($command, $output, $returnVar);
+
+echo "実行結果:<br>";
 print_r($output);
+echo "<br>終了ステータス: " . $returnVar;
