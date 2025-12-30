71788959e52dc387f49137d96a764913b908c8c2
diggy <nitayama.biz@gmail.com>
2025-12-30 15:13:36 +0900

    c

--- public/corn.php
+++ public/corn.php
@@ -1,11 +1,9 @@
 <?php
-// ロリポップ PHP8.4 の正確なパスを指定
+// ロリポップ PHP8.4 の正確なパス
 $phpPath = '/usr/local/php/8.4/bin/php';
 $artisanPath = '/home/users/1/littlestar.jp-proud-takeo-0732/web/hamro-life-japan.com/artisan';
-$command = "{$phpPath} {$artisanPath} fetch:nepal-news 2>&1";
 
-exec($command, $output, $returnVar);
+// 実行
+exec("{$phpPath} {$artisanPath} fetch:nepal-news 2>&1", $output);
 
-echo "実行結果:<br>";
 print_r($output);
-echo "<br>終了ステータス: " . $returnVar;
