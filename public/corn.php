ef6998ec07fc31db5431670d052696d4455cd78c
diggy <nitayama.biz@gmail.com>
2025-12-30 15:17:23 +0900

    ccccc

--- public/corn.php
+++ public/corn.php
@@ -3,7 +3,13 @@
 $phpPath = '/usr/local/php/8.4/bin/php';
 $artisanPath = '/home/users/1/littlestar.jp-proud-takeo-0732/web/hamro-life-japan.com/artisan';
 
-// 実行
-exec("{$phpPath} {$artisanPath} fetch:nepal-news 2>&1", $output);
+// PHP_BINARY 環境変数を書き換えて、Laravel内部の誤認を強制修正する
+putenv("PHP_BINARY={$phpPath}");
 
+// 直接 artisan を叩くのではなく、phpコマンドの引数として渡す
+$command = "{$phpPath} {$artisanPath} fetch:nepal-news 2>&1";
+
+exec($command, $output);
+
+echo "--- 実行ログ ---<br>";
 print_r($output);
