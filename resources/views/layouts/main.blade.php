fdd664e1644bcdabf6260f738f8186c8d41d0fd1
diggy <nitayama.biz@gmail.com>
2026-01-01 11:43:22 +0900

    favi

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', '就労支援サービス')</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
     <style>
         body {
             background-color: #f6f7fa;
