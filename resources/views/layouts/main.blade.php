b7f9617c5bcb9b465a60cc200f559f20a201af38
“diggi” <nitayama.biz@gmail.com>
2026-02-09 01:48:09 +0900

    script error

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1600,7 +1600,7 @@
                     @endif
                     <div class="ad-slide">
                         {!! '<a href="https://rpx.a8.net/svt/ejp?a8mat=45KOC7+DTQEIA+2HOM+62U35&rakuten=y&a8ejpredirect=http%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2F0ea62065.34400275.0ea62066.204f04c0%2Fa25122762590_45KOC7_DTQEIA_2HOM_62U35%3Fpc%3Dhttp%253A%252F%252Fwww.rakuten.co.jp%252F%26m%3Dhttp%253A%252F%252Fm.rakuten.co.jp%252F" rel="nofollow">
-<img src="http://hbb.afl.rakuten.co.jp/hsb/0eb4bbdb.d3e5aa19.0eb4bbaa.95151395/" border="0"></a>
+<img src="https://hbb.afl.rakuten.co.jp/hsb/0eb4bbdb.d3e5aa19.0eb4bbaa.95151395/" border="0"></a>
 <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=45KOC7+DTQEIA+2HOM+62U35" alt="">' !!}
                     </div>
                     @if($hasAd2)
